a6dba4efc03c301ea219bcadd3b1681c2188615c
Harishdzukroni <138541978+Harishdzukroni@users.noreply.github.com>
2026-02-04 15:42:10 +0700

    Add files via upload

---
+++ firebase-init.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBkpmVvo5o_NPmFZvtRarun1HAtLPYjWNE",
+  authDomain: "webcaturuinsa.firebaseapp.com",
+  projectId: "webcaturuinsa",
+  storageBucket: "webcaturuinsa.firebasestorage.app",
+  messagingSenderId: "994496774365",
+  appId: "1:994496774365:web:a4a017ac667404b08d1b34",
+  measurementId: "G-JKZKB4LP0Y"
+};
