a5b422ae2d1682c2a6a5f68c4a848f1090ba8641
Harishdzukroni <138541978+Harishdzukroni@users.noreply.github.com>
2026-02-05 12:40:40 +0700

    Add Firebase initialization and configuration
    Added Firebase initialization code and comments.

--- firebase-init.js
+++ firebase-init.js
@@ -8,3 +8,16 @@ const firebaseConfig = {
   appId: "1:994496774365:web:a4a017ac667404b08d1b34",
   measurementId: "G-JKZKB4LP0Y"
 };
+// Konfigurasi dari Firebase Console
+const firebaseConfig = {
+  apiKey: "AIzaSyBkpmVvo5o_NPmFZvtRarun1HAtLPYjWNE",
+  authDomain: "webcaturuinsa.firebaseapp.com",
+  projectId: "webcaturuinsa",
+  storageBucket: "webcaturuinsa.firebasestorage.app",
+  messagingSenderId: "994496774365",
+  appId: "1:994496774365:web:a4a017ac667404b08d1b34",
+  measurementId: "G-JKZKB4LP0Y"
+};
+
+// [PENTING] Baris ini wajib ada agar Firebase aktif!
+firebase.initializeApp(firebaseConfig);
